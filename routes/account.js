1cca146872473eb652ef3f5bff52b2ff6861547d
Evgeniy <e_danilchenko@outlook.com>
2020-03-05 13:54:58 +0200

    Add user model

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.get('/reg', (req, res) => {
-    res.send('Registration page');
+// router.get('/reg', (req, res) => {
+//     res.send('Registration page');
+// });
+
+router.post('/reg', (req, res) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        login: req.body.login,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err)
+            res.json({success: false, msg: 'User not add!'});
+        else
+            res.json({success: true, msg: 'User add!'});
+    });
 });
 
 router.get('/auth', (req, res) => {
