b259b479c052cfc4cbcf7cf0670c681564910a80
Evgeniy <e_danilchenko@outlook.com>
2020-03-10 19:05:57 +0200

    Add JWT users auth

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/db');
 
 // router.get('/reg', (req, res) => {
 //     res.send('Registration page');
@@ -26,7 +29,41 @@ router.get('/auth', (req, res) => {
     res.send('Auth page');
 });
 
-router.get('/dashboard', (req, res) => {
+router.post('/auth', (req, res) => {
+    const login = req.body.login;
+    const password = req.body.password;
+
+    User.getUserByLogin(login, (err, user) => {
+        if(err) 
+            throw err;
+        if(!user)
+            return res.json({success: false, msg: 'User not be found'});
+        
+        User.comparePass(password, user.password, (err, isMatch) => {
+            if(err) throw err;
+            if(isMatch){
+                const token = jwt.sign(user, config.secret, {
+                    expiresIn: 3600 * 24
+                });
+                res.json({
+                    success: true,
+                    token: 'JWT ' + token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        login: user.login,
+                        email: user.email
+                    }
+                })
+            }
+            else{
+                return res.json({success: false, msg: 'Passwords not match'});
+            }
+        });
+    });
+})
+
+router.get('/dashboard', passport.authenticate('jwt', {session: false}), (req, res) => {
     res.send('User cabinet');
 });
 
